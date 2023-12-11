33ecf77cecc05620a1fe982a6ecce50ff4e1e9d6
Sam Forrest <samforrest@live.co.uk>
2023-12-11 20:06:44 +0000

    AMEND: Updated mobile styling/functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,55 @@
 <script>
+  const mq = window.matchMedia("(min-width: 992px)");
   let completed = false;
   let handleMouseMove;
   let completeSurvey;
+  let failSurvey;
   let x = 0;
   let y = 0;
+  let yesBtnFullWidth = false;
 
   document.addEventListener('DOMContentLoaded', () => {
-    const minimumDistance = 50; // Set your desired minimum distance here
     const target = document.querySelector('.__no');
-
+    
     completeSurvey = () => {
       completed = true;
     }
-
-    handleMouseMove = (event) => {
-      const cursorX = event.clientX;
-      const cursorY = event.clientY;
-
+    
+    if (mq.matches) {
+      const minimumDistance = 50; // Set your desired minimum distance here
       const targetRect = target.getBoundingClientRect();
       const targetX = targetRect.left + targetRect.width / 2;
-      const targetY = targetRect.top + targetRect.height / 2;
+      let targetY = targetRect.top + targetRect.height / 2;
 
-      const distance = Math.sqrt(
-        Math.pow(cursorX - targetX, 2) + Math.pow(cursorY - targetY, 2)
-      );
+      handleMouseMove = (event) => {
+        const cursorX = event.clientX;
+        const cursorY = event.clientY;
+
+        const distance = Math.sqrt(
+          Math.pow(cursorX - targetX, 2) + Math.pow(cursorY - targetY, 2)
+        );
 
 
-      if (distance < 100) {
-        const dx = cursorX - targetX;
-        const dy = cursorY - targetY;
-
-        const length = Math.sqrt(dx * dx + dy * dy);
-        const normalizedDx = -dx / length; // Negate here
-        const normalizedDy = -dy / length; // Negate here
-
-        const adjustedDistance = Math.max(minimumDistance, distance);
-
-        x = normalizedDx * adjustedDistance;
-        y = normalizedDy * adjustedDistance;
+        if (distance < 100) {
+          const dx = cursorX - targetX;
+          const dy = cursorY - targetY;
+          
+          const length = Math.sqrt(dx * dx + dy * dy);
+          const normalizedDx = -dx / length; // Negate here
+          const normalizedDy = -dy / length; // Negate here
+          
+          const adjustedDistance = Math.max(minimumDistance, distance);
+          
+          x = normalizedDx * adjustedDistance;
+          y = normalizedDy * adjustedDistance;
+        }
+      };
+    } else {
+      failSurvey = () => {
+        y = 5000;
+        yesBtnFullWidth = true;
       }
-    };
+    }
   });
 </script>
 
@@ -60,16 +70,16 @@
           <p class="result __no">0%</p>
         </div><!-- col -->
       {:else}
-        <div class="col-lg-4 offset-lg-2">
+        <div class="col-lg-4 offset-lg-2 my-3 my-lg-0">
           <div class="image-wrap">
             <img src="/die-hard.jpg" alt="Die Hard movie poster">
           </div>
         </div><!-- col -->
-        <div class="col-lg-4 d-flex flex-column justify-content-center align-items-center">
+        <div class="col-lg-4 d-flex flex-column justify-content-center align-items-center mb-3 mb-lg-0">
           <h1 class="title">Is Die Hard a Christmas Movie?</h1>
           <div class="buttons">
-            <button class="button __yes" on:click={completeSurvey}>Yes</button>
-            <button class="button __no" style={`transform: translate(${x}px, ${y}px);`}>No</button>
+            <button class={`button __yes ${yesBtnFullWidth ? '__fullwidth' : ''}`} on:click={completeSurvey}>Yes</button>
+            <button class="button __no" on:click={failSurvey} style={`transform: translate(${x}px, ${y}px);`}>No</button>
           </div>
         </div>
       {/if}
@@ -126,6 +136,12 @@
   .button.__yes {
     background: var(--color-green);
     margin-right: 104px;
+    max-width: 92px;
+    width: 100%;
+  }
+
+  .button.__yes.__fullwidth {
+    max-width: 200px;
   }
 
   .button.__no {
@@ -151,5 +167,11 @@
 
   .result.__no {
     color: var(--color-red);
+  }
+
+  @media screen and (max-width: 992px) {
+      .result {
+        margin-bottom: 2rem;
+      }
   }
 </style>
